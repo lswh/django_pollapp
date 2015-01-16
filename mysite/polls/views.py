ad201ddfcde7119dd860836266ba4f936dca0a2c
heleyna <labao.hm@gmail.com>
2015-01-16 21:45:20 +0800

    generic views used to shorten code and commented out old long code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,41 +2,29 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
+from django.views import generic
 
 from polls.models import Choice, Poll
 
 # Create your views here.
-def index(request):
-	''' Long Method Outlined below
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-    	'latest_poll_list': latest_poll_list,
-    })
-	return HttpResponse(template.render(context))'''
-	#This is the shorter method outlined on the tutorial for rendering same view
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	context = {'latest_poll_list': latest_poll_list}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, poll_id):
-	#return HttpResponse("You're looking at poll %s." % poll_id)
-	#Long Method
-	'''try:
-		poll = Poll.objects.get(pk=poll_id)
-	except Poll.DoesNotExist:
-		raise Http404
-	return render(request, 'polls/detail.html', {'poll': poll})'''
-	#Shortened method
-	poll = get_object_or_404(Poll, pk=poll_id)
-	return render(request, 'polls/detail.html', {'poll': poll})
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
 
 
-def results(request, poll_id):
-	#return HttpResponse("You're looking at the results of poll %s." % poll_id)
-	poll = get_object_or_404(Poll, pk=poll_id)
-	return render(request, 'polls/results.html', {'poll': poll})
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
 
 def vote(request, poll_id):
 	# return HttpResponse("You're voting on poll %s." % poll_id)
@@ -54,3 +42,36 @@ def vote(request, poll_id):
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
+### LONG METHODS PREVIOUSLY CODED FROM TUTORIAL BELOW ###
+
+# def index(request):
+	#latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	#template = loader.get_template('polls/index.html')
+	#context = RequestContext(request, {
+    #	'latest_poll_list': latest_poll_list,
+    #})
+	#return HttpResponse(template.render(context))
+	#This is the shorter method outlined on the tutorial for rendering same view
+	#latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	#context = {'latest_poll_list': latest_poll_list}
+	#return render(request, 'polls/index.html', context)
+
+
+
+# def detail(request, poll_id):
+	#return HttpResponse("You're looking at poll %s." % poll_id)
+	#Long Method
+	#try:
+	#	poll = Poll.objects.get(pk=poll_id)
+	#except Poll.DoesNotExist:
+	#	raise Http404
+	#return render(request, 'polls/detail.html', {'poll': poll})
+	#Shortened method
+	#poll = get_object_or_404(Poll, pk=poll_id)
+	#return render(request, 'polls/detail.html', {'poll': poll})
+
+
+# def results(request, poll_id):
+	#return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	#poll = get_object_or_404(Poll, pk=poll_id)
+	#return render(request, 'polls/results.html', {'poll': poll})
