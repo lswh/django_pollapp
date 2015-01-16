0462b909d662b735a47f7ca0f01587a5af04dfe4
heleyna <labao.hm@gmail.com>
2015-01-16 21:53:58 +0800

    added test cases in python tests for poll

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,10 +9,12 @@ class Poll(models.Model):
     def __unicode__(self):
     	return self.question
     def was_published_recently(self):
-    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    	was_published_recently.admin_order_field = 'pub_date'     	
-    	was_published_recently.boolean = True
-    	was_published_recently.short_description = 'Published recently?'
+    	#return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    	#was_published_recently.admin_order_field = 'pub_date'     	
+    	#was_published_recently.boolean = True
+    	#was_published_recently.short_description = 'Published recently?'
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
