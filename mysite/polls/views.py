a107dd9e0715b0478bcbbe4ae580bfc71bb2ba8c
heleyna <labao.hm@gmail.com>
2015-01-16 21:18:06 +0800

    templates and namespacing tutorial part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello world, you're at the poll index.")
+	''' Long Method Outlined below
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {
+    	'latest_poll_list': latest_poll_list,
+    })
+	return HttpResponse(template.render(context))'''
+	#This is the shorter method outlined on the tutorial for rendering same view
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
+
 
 def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
+	#return HttpResponse("You're looking at poll %s." % poll_id)
+	#Long Method
+	'''try:
+		poll = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+	return render(request, 'polls/detail.html', {'poll': poll})'''
+	#Shortened method
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, 'polls/detail.html', {'poll': poll})
+
 
 def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
